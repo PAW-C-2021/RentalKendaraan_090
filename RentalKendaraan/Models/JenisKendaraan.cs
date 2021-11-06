89bb6995969670d71341bb9c45bebec911ef486d
aqfa11 <80084765+aqfa11@users.noreply.github.com>
2021-11-06 21:37:21 +0700

    Add project files.

---
+++ RentalKendaraan/Models/JenisKendaraan.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace RentalKendaraan.Models
+{
+    public partial class JenisKendaraan
+    {
+        public int IdJenisKendaraan { get; set; }
+        public string NamaJenisKendaraan { get; set; }
+
+        public virtual Kendaraan Kendaraan { get; set; }
+    }
+}
